79df57f130eb929825905c68b8126899d506699f
isenTang <3104255643@qq.com>
2020-08-04 16:27:08 +0800

    作业

--- 第三阶段/day02/index.js
+++ 第三阶段/day02/index.js
@@ -268,30 +268,88 @@ const { globalAgent } = require('http');
 //     console.log(item);
 // })
 
-let map = new Map();
+// let map = new Map();
 
 // let obj = {
-//     {}:'a'
+//     a:1
 // }
 
-let obj = {
-    a:1
-}
+// let a = 1;
 
-let a = 1;
+// console.log(obj);
+// map.set(obj,'1234');
+// map.set(a,'1111')
 
-console.log(obj);
-map.set(obj,'1234');
-map.set(a,'1111')
+// console.log(map);
 
-console.log(map);
+// console.log(map.get(obj));
 
-console.log(map.get(obj));
+// console.log(map.size);
 
-console.log(map.size);
+// console.log(map.values());
 
-console.log(map.values());
+// map.delete(obj);
 
-map.delete(obj);
+// console.log(map);
 
-console.log(map);
+// let map = new Map();
+// let array = [2, 3, 5, 4, 5, 2, 2, 2, 2, 2, 2, 2, 2,2];
+
+// array.forEach((item)=>{
+
+//     // 如果map里面没有
+//     if(!map.get(item)){
+
+//         //新插入一个key-value值，代表第一次遇到这个数字
+//         map.set(item,1);
+//     }else{
+//         // 如果数字已经存在，获取之前计算的次数，然后+1
+//         map.set(item,map.get(item)+1);
+//     }
+// })
+
+// console.log(map);
+
+// [
+//     {2:2},
+//     {1:2}
+// ]
+
+// const numbers = [1,2,2,3];
+
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i]);
+// }
+
+// for(let num of numbers){
+//     console.log(num);
+  
+//   	if(num === 2){
+//       break;
+//     }
+// }
+
+let array = [{a:1},{a:2}]
+
+// for(let item of array){
+
+//     item.b = 1;
+// }
+
+// console.log(array);
+
+let numbers = [1,2,3,5];
+// map，返回一个新生成的数组
+array = array.map((item)=>{
+
+    console.log(item);
+    item.b = 2;
+
+    return item;
+})
+
+// console.log(array)
+
+// numbers = numbers.map(item=>item*item)
+
+// console.log(numbers);
