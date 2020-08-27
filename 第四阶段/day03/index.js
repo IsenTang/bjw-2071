1cbfdc517036bcbfe525a40ab27ece30c936dc85
isenTang <3104255643@qq.com>
2020-08-27 18:51:47 +0800

    资料

--- 第四阶段/day03/index.js
+++ 第四阶段/day03/index.js
@@ -35,16 +35,16 @@ let person2 = {
 // person.sayName.call(person2)
 
 
-let person3 = {
-    name: 'tom'
-}
+// let person3 = {
+//     name: 'tom'
+// }
 
-function fakeSayName(v){
+// function fakeSayName(v){
 
-    console.log(this.name + v);
-}
+//     console.log(this.name + v);
+// }
 
-global.name = 'global'
+// global.name = 'global'
 
 // fakeSayName.call(person3,1)
 
@@ -164,7 +164,7 @@ global.name = 'global'
 //     }
 // }
 
-// let obj2 = Object.assign({a:1},obj1);
+// let obj2 = Object.assign({},obj1);
 
 // obj2.age = 18;
 // console.log(obj1);
@@ -174,17 +174,17 @@ global.name = 'global'
 // console.log(obj2);
 
 
-let obj1 = {
-    name:'isen',
-    age:28,
-    obj:{
-        a:1
-    },
-    func: function(){
+// let obj1 = {
+//     name:'isen',
+//     age:28,
+//     obj:{
+//         a:1
+//     },
+//     func: function(){
 
-        console.log(1);
-    }
-}
+//         console.log(1);
+//     }
+// }
 
 // let obj2 = JSON.parse(JSON.stringify(obj1))
 
@@ -199,18 +199,54 @@ let obj1 = {
 // console.log(obj1);
 // console.log(obj2)
 
-let obj2 = _.cloneDeep(obj1);
+// let obj1 = {
+//     name:'isen',
+//     age:28,
+//     obj:{
+//         a:1
+//     },
+//     func: function(){
 
-obj2.age = 18;
-obj2.obj.a = 3;
-console.log(obj1);
-console.log(obj2)
+//         console.log(1);
+//     }
+// }
 
-obj2.func = function(){
+// let obj2 = _.cloneDeep(obj1);
 
-    console.log(2);
-}
-obj1.func()
-obj2.func()
+// obj2.age = 18;
+// obj2.obj.a = 3;
+// console.log(obj1);
+// console.log(obj2)
 
+// obj2.func = function(){
 
+//     console.log(2);
+// }
+// obj1.func()
+// obj2.func()
+
+// let a = 1; 
+// let b = 2; 
+// let test = function (first, second) { 
+//     return first === a && second === b;
+// }; 
+
+// console.log(test(1,2));
+// console.log(fakeCall(test, a, b,33))
+
+// function fakeCall(fn){
+
+//     // console.log(arguments);
+//     // return fn(a,b)
+
+//     // return fn.call(null,a,b)
+//     // return fn.apply(null,[a,b])
+//     let array = [];
+
+//     for(let i = 1;i<arguments.length;i++){
+//         array.push(arguments[i])
+//     }
+    
+//     // console.log(array);
+//     return fn.apply(null,array)
+// }
