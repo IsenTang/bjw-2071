6fea1fb7bda1d7a318dab6eacd8871880923acc5
isenTang <3104255643@qq.com>
2020-08-31 15:13:57 +0800

    storage

--- 第四阶段/day05/index.js
+++ 第四阶段/day05/index.js
@@ -78,20 +78,99 @@ function getPromise(){
     
 // }
 
-setTimeout(function () {
-    console.log(1);
-},0);
+// setTimeout(function () {
+//     console.log(1);
+// },0);
 
-new Promise(function(resolve,reject){
-    console.log(2)
-    resolve(3)
-}).then(function(val){
-    console.log(val);
-})
+// new Promise(function(resolve,reject){
+//     console.log(2)
+//     resolve(3)
+// }).then(function(val){
+//     console.log(val);
+// })
 
-new Promise(function(resolve,reject){
-    console.log(22)
-    resolve(33)
-}).then(function(val){
-    console.log(val);
-})
+// new Promise(function(resolve,reject){
+//     console.log(22)
+//     resolve(33)
+// }).then(function(val){
+//     console.log(val);
+// })
+
+// function main(){
+//     console.log('main');
+//     a();
+// }
+
+
+// function a(){
+//     console.log('a');
+//     b();
+// }
+
+// function b(){
+//     console.log('b');
+    
+//     setTimeout(function(){
+        
+//         console.log(' timeout ');
+//     },0)
+    
+//     console.log(' b done !');
+// }
+
+// main()
+
+// console.log(1)
+
+// setTimeout(() => {
+//   console.log(2)
+// }, 0)
+
+// Promise.resolve().then(() => {
+// 	console.log(3)
+// }).then(() => {
+// 	console.log(4)
+// })
+
+// console.log(5)
+
+// async function async1(){
+//     console.log('async1 start')
+//     async2().then(()=>{
+//         console.log('async1 end')
+//     })
+    
+// }
+// async function async2(){
+//     console.log('async2')
+// }
+
+// console.log('script start')
+
+// setTimeout(function(){
+//     console.log('setTimeout') 
+// },0)  
+
+// async1();
+
+// new Promise(function(resolve){
+//     console.log('promise1')
+//     resolve();
+// }).then(function(){
+//     console.log('promise2')
+// })
+// console.log('script end')
+
+
+// async function main(){
+
+//     console.log(1);
+// }
+
+// main().then(()=>{
+
+//     console.log(2);
+// })
+
+const string = JSON.stringify({name:'isen'});
+console.log(JSON.parse(string));
