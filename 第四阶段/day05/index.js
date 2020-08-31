5d0c03bb296479d2c879b811ba515e5e3a225df5
isenTang <3104255643@qq.com>
2020-08-31 09:42:19 +0800

    事件循环

---
+++ 第四阶段/day05/index.js
@@ -0,0 +1,41 @@
+// function main(){
+//     console.log('main');
+//     a();
+// }
+
+// function a(){
+//     console.log('a');
+//     b();
+// }
+
+// function b(){
+    
+//     console.log('b')
+// }
+
+// main();
+
+// console.log("Hi!");
+
+// setTimeout(function timeout() {
+//     console.log("Click the button!");
+// }, 0);
+
+// console.log("Welcome to loupe.");
+console.log("Hi!");
+
+setTimeout(function timeout() {
+    console.log("Click the button!");
+}, 2000);
+setTimeout(function timeout() {
+    console.log("setTimeout1");
+}, 1000);
+setTimeout(function timeout() {
+    console.log("setTimeout2");
+}, 3000);
+
+
+console.log("Welcome to loupe.");
+console.log("Welcome to loupe.");
+console.log("Welcome to loupe.");
+console.log("Welcome to loupe.");
