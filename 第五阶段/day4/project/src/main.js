d7b9fce5127615c8ac909f741c5489f907b33dcf
JiangWen <13089848409@163.com>
2020-09-10 16:21:17 +0800

    day4

--- 第五阶段/day4/project/src/main.js
+++ 第五阶段/day4/project/src/main.js
@@ -1,7 +1,16 @@
 import Vue from "vue";
 import App from "./App.vue";
+import Config from "./utils/config"
 
 Vue.config.productionTip = false;
+
+
+Vue.directive('oss-src', function (el, binding) {
+  let imgURL = binding.value;//获取图片地址
+  if (imgURL) el.setAttribute('src',
+  Config.ossPath + imgURL);
+})
+
 
 new Vue({
   render: h => h(App)
