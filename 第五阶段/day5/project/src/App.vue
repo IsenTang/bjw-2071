dc5b050e27dd98fb611fdf4eb0bd596bebdad563
JiangWen <13089848409@163.com>
2020-09-11 17:20:52 +0800

    作业

---
+++ 第五阶段/day5/project/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+  <button @click="closeHomePage">关掉这个home页面</button>
+    <Home v-if="isShowHome"></Home>
+  </div>
+</template>
+
+<script>
+import Home from "./components/home.vue";
+
+export default {
+  name: "App",
+  data () {
+    return {
+      isShowHome: true
+    }
+  },
+  components: {
+    Home
+  },
+  methods: {
+    closeHomePage () {
+      this.isShowHome = !this.isShowHome;
+    }
+  },
+};
+</script>
