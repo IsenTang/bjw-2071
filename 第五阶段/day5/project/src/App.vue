b3c6297d57184fc7e2745b3efd6a5658fabf1f70
JiangWen <13089848409@163.com>
2020-09-14 10:30:49 +0800

    作业解析

--- 第五阶段/day5/project/src/App.vue
+++ 第五阶段/day5/project/src/App.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
-  <button @click="closeHomePage">关掉这个home页面</button>
-    <Home v-if="isShowHome"></Home>
+    <!--<button @click="closeHomePage">关掉这个home页面</button>
+    <Home v-if="isShowHome"></Home> -->
+    <Work></Work>
   </div>
 </template>
 
 <script>
 import Home from "./components/home.vue";
-
+import Work from "./components/work"
 export default {
   name: "App",
   data () {
@@ -16,7 +17,7 @@ export default {
     }
   },
   components: {
-    Home
+    Home, Work
   },
   methods: {
     closeHomePage () {
---
+++ 第五阶段/day6/day6pro/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div style="text-align: center; position: relative;">
+    <button @click="click1">btn1</button>
+  </div>
+</template>
+
+<script>
+import Common from "./mixins/common";
+export default {
+  name: "App",
+  mixins: [Common.myMixin],
+  data() {
+    return {
+      a: "aaa",
+      message: "我是父组件的数据"
+    };
+  },
+  methods: {
+    click1() {
+      console.log("mixin message", this.message);
+      this.sayHello();
+    }
+  }
+};
+</script>
