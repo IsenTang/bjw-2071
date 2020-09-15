ff0257192a1c3bca0dd8d5b934fb946233424264
JiangWen <13089848409@163.com>
2020-09-15 10:36:27 +0800

    less

--- 第五阶段/day7/abc/src/components/HelloWorld.vue
+++ 第五阶段/day7/abc/src/components/HelloWorld.vue
@@ -1,13 +1,35 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
+    <img alt="Vue logo" class="img2" src="../assets/logo.png" />
+    <h1 class="hhh1">{{ msg }}</h1>
+    <p class="ppp">
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
+      <a class="a1" href="https://cli.vuejs.org" target="_blank" rel="noopener"
+        >vue-cli documentation
+        <span>span</span>
+        </a>
+      <a class="a2" href="https://cli.vuejs.org" target="_blank" rel="noopener"
+        >vue-cli documentation</a>
+      <a class="a3" href="https://cli.vuejs.org" target="_blank" rel="noopener"
+        >vue-cli documentation</a>
+      <a class="a4" href="https://cli.vuejs.org" target="_blank" rel="noopener"
+        >vue-cli documentation</a>
+      <a class="a5" href="https://cli.vuejs.org" target="_blank" rel="noopener"
+        >vue-cli documentation</a>
     </p>
+    <br>
+    <div class="box">
+      <h2>标题</h2>
+      <div class="info1">
+          <input type="text" />
+          <p>没class的p</p>
+          <p class="font">有class的p</p>
+      </div>
+      <div class="info2">
+          <img src="path" />
+      </div>
+    </div>
     <h3>Installed CLI Plugins</h3>
     <ul>
       <li>
@@ -96,19 +118,66 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style scoped lang="less">
+@import url(../style/style.less);
+@hjs: #f97197;
+@opa: 0.5;
+@font-color: #f97197;
+
+.hhh1{
+  font-size: 20px;
+  color: @hjs;
+  opacity: @opa;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.ppp{
+  font-size: 14px;
+  .a1{
+    color: red;
+    span{
+      color: #333;
+    }
+  }
+  .a2{
+    color: blue;
+  }
+  .a3{
+    color: sienna;
+  }
+  .a4{
+    color: purple;
+  }
+  .a5{
+    color: gold;
+  }
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.box{
+  background: #f3f3f3;
+  width: 200px+200px;
+  height: 500px-200;
+  border-width: 2*2px;
+  border-style: solid;
+  border-color: @font-color;
+  box-shadow: 0 2/2px 10px #000;
+  h2{
+    font-size: 20px;
+    color: #f97197;
+    color: lighten(@font-color, 10%);
+  }
+  .info1{
+    input{}
+    .font{
+      font-weight: bold;
+      color: red;
+    }
+    p{
+      font-size: 30px;
+      color: springgreen;
+    }
+  }
+  .info2{
+    img{}
+  }
 }
 </style>
