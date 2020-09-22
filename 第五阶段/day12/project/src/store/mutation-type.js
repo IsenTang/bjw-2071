6cf8118dd6ff9e8207588162d9b77b8cb3cbab22
JiangWen <13089848409@163.com>
2020-09-22 16:40:44 +0800

    work

---
+++ 第五阶段/day12/project/src/store/mutation-type.js
@@ -0,0 +1,14 @@
+/** 更新了login的状态 */
+export const UPDATE_LOGIN = 'updateLogin';
+
+/** 更新了list */
+export const UPDATE_List = 'updateList';
+
+/** 更新用户信息*/
+export const UPDATE_USER_INFO = 'updateUserInfo';
+
+/** 更新了list */
+export const UPDATE_List3 = 'updateList3';
+
+/** 更新了list */
+export const UPDATE_List4 = 'updateList4';
--- 第五阶段/day12/project/src/views/Home.vue
+++ 第五阶段/day12/project/src/views/Home.vue
@@ -3,26 +3,38 @@
     <h2>是否登录{{ isLogin }}</h2>
     <h2>小鲜肉：{{ list }}</h2>
     <button @click="fun">更新登录状态</button>
+
+    <div>
+      <ul>
+        <li>{{ user.name }}</li>
+        <li>{{ user.age }}</li>
+        <li>{{ user.sex }}</li>
+        <li>{{ user.school }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
-import { mapState, mapMutations } from "vuex"
+import { mapState, mapMutations, mapActions } from "vuex"
 export default {
   name: "Home",
   computed: {
     ...mapState({
       "isLogin": state => state.isLogin,
-      "list": state => state.list
+      "list": state => state.list,
+      "user": state => state.user
     })
   },
   methods: {
-    ...mapMutations([
-      'updateLogin', 'updateList'
+    // ...mapMutations([
+    //   'updateLogin', 'updateList'
+    // ]),
+    ...mapActions([
+      'updateLoginState'
     ]),
     fun() {
-      let list = [ { name: '赵丽颖' }, { name: '杨幂' },
-        { name: '迪丽热巴' }, { name: '古力娜扎' }]
-      this.updateList(list);
+    //   this.updateList(list);
+      this.updateLoginState(true);
     }
   },
 };
