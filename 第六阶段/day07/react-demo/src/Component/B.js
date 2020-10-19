cbe56573c6e2ab3de1c24f9a9b6ef77477262462
isenTang <3104255643@qq.com>
2020-10-19 14:24:35 +0800

    router nest

--- 第六阶段/day07/react-demo/src/Component/B.js
+++ 第六阶段/day07/react-demo/src/Component/B.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from 'react';
+import {
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 export default function B() {
     return (
         <div>
-            B
+            <div>B</div>
+            
+            <div>
+
+                <div>
+
+                    <ul>
+                        <li>
+                            <Link to={'/b/index'}>home</Link>
+                        </li>
+                        <li>
+                            <Link to={'/b/helloworld'}>helloworld</Link>
+                        </li>
+                    </ul>
+                </div>
+
+                <div>
+
+                    <Switch>
+
+                        <Route path={'/b/index'}>
+                            <div>
+                                b home
+                            </div>
+                        </Route>
+
+                        <Route path={'/b/helloworld'}>
+                            <div>
+                                helloworld
+                            </div>
+                        </Route>
+                    </Switch>
+                </div>
+            </div>
         </div>
     )
 }
